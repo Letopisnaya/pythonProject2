2845bfc6363261445503da8485192b2e663f4828
Ann Lyapunova <lord.letopisniy@yandex.ru>
2024-08-31 13:25:58 +0300

    add new file for next functions

--- src/widget.py
+++ src/widget.py
@@ -1,21 +1,30 @@
 from src.masks import get_mask_card_number
+from src.masks import get_mask_account
 
 
-def mask_account_card(number: str) -> str:
-    """Функция маскирует счета и номера карт"""
-    if len(number.split()[-1]) == 16:
-        new_number = card_number(number.split()[-1])
-        result = f"{number[:16]}{new_number}"
-    elif len(number.split()[-1]) == 20:
-        new_number = get_mask_card_number(number.split()[-1])
-        result = f"{number[:-20]}{new_number}"
-    return result
+def mask_account_card(type_and_number: str) -> str:
+    """Функция принимает тип и номер карты или счета и возвращает замаскированный номер"""
+    divide_string = type_and_number.split(" ")
+    type_card = []
+
+    for i in divide_string:
+        if i == "Счет":
+            return f"{divide_string[0]} {get_mask_account(divide_string[-1])}"
+        else:
+            type_card.append(i)
+
+    type_card_name = " ".join(type_card[:-1])
+    return f"{type_card_name} {get_mask_card_number(divide_string[-1])}"
 
 
-def get_data(old_date: str) -> str:
-    """Функция преобразования даты"""
-    return f"{old_date[8:10]}.{old_date[5:7]}.{old_date[0:4]}"
+user_input = input()
+print(mask_account_card(user_input))
 
 
+def get_date(format_data: str) -> str:
+    """Функция преобразует формат даты"""
+    return f"{format_data[8:10]}.{format_data[5:7]}.{format_data[:4]}"
 
 
+user_input_data = input()
+print(get_date(user_input_data))
